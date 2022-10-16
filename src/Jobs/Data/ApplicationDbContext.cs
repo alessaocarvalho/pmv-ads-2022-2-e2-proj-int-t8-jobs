452754864c4865a307520925337b7fd056554847
RaulShinaede <rshinaede@outlook.com>
2022-10-16 17:54:59 -0300

    Adicionando projeto da aplicação web mvc

---
+++ src/Jobs/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Jobs.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jobs.Data {
+    public class ApplicationDbContext : DbContext {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
