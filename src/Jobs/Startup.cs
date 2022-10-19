89c6179ed4a9cccdde363b894c5ec063c884a739
RaulShinaede <rshinaede@outlook.com>
2022-10-19 20:22:56 -0300

    Removendo comentários

--- src/Jobs/Startup.cs
+++ src/Jobs/Startup.cs
@@ -21,7 +21,6 @@ namespace Jobs {
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
@@ -33,7 +32,6 @@ namespace Jobs {
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
@@ -41,7 +39,6 @@ namespace Jobs {
             }
             else {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
