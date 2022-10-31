088b51edb19931d088a2d1ccadff28e6f3691831
RaulShinaede <rshinaede@outlook.com>
2022-10-30 21:50:45 -0300

    Atualizando suporte ao docker

--- src/Jobs/Startup.cs
+++ src/Jobs/Startup.cs
@@ -1,5 +1,6 @@
 using Jobs.Data;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
@@ -10,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -30,6 +32,11 @@ namespace Jobs {
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
+
+            var pgrmData = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+            Directory.CreateDirectory($"{pgrmData}\\myaspnetwebapp\\keys");
+            services.AddDataProtection()
+                .PersistKeysToFileSystem(new DirectoryInfo($"{pgrmData}\\myaspnetwebapp\\keys"));
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
@@ -43,6 +50,21 @@ namespace Jobs {
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+
+            try {
+
+
+
+                using (var scope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+                using (var ctx = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>()) {
+                    ctx.Database.EnsureCreated();
+                    ctx.Database.Migrate();
+                }
+
+            }
+            catch {
+
+            }
 
             app.UseRouting();
 
